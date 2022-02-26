80cac40e4f1ceba6325ded7181217a512728a6f9
Afolayan Stephen <afolayan4life@gmail.com>
2022-02-26 13:34:12 +0100

    Fixed font-family names

--- pages/index.js
+++ pages/index.js
@@ -15,98 +15,84 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className={styles.container}>
-          {/* <nav className={styles.navbar}>
-            <Image src="/logo.png" alt="Jilh Logo" width={45} height={45} />
-            <div className={styles.menu}>
-              <div className={styles.links}>
-                <Link href="#"><a>Home</a></Link>
-                <Link href="#"><a>Projects</a></Link>
-                <Link href="#"><a>Talks</a></Link>
-                <Link href="#"><a>Resume</a></Link>
-              </div>
-              <IoMenuOutline size={40}/>
-            </div>
-          </nav> */}
-        </div>
         <div className={styles.pageContainer}>
-        <div className={styles.container}>
-        <nav className={styles.navbar}>
-            <Image src="/logo.png" alt="Jilh Logo" width={45} height={45} />
-            <div className={styles.menu}>
-              <div className={styles.links}>
-                <Link href="#"><a>Home</a></Link>
-                <Link href="#"><a>Projects</a></Link>
-                <Link href="#"><a>Talks</a></Link>
-                <Link href="#"><a>Resume</a></Link>
+          <div className={styles.container}>
+            <nav className={styles.navbar}>
+              <Image src="/logo.png" alt="Jilh Logo" width={45} height={45} />
+              <div className={styles.menu}>
+                <div className={styles.links}>
+                  <Link href="#"><a>Home</a></Link>
+                  <Link href="#"><a>Projects</a></Link>
+                  <Link href="#"><a>Talks</a></Link>
+                  <Link href="#"><a>Resume</a></Link>
+                </div>
+                <IoMenuOutline size={40} />
               </div>
-              <IoMenuOutline size={40}/>
-            </div>
-          </nav>
-          <div className={styles.pageContent}>
-            <div className={styles.infoSection}>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-            </div>
-            <div className={styles.projectSection}>
+            </nav>
+            <div className={styles.pageContent}>
+              <div className={styles.infoSection}>
+                <h1 className={styles.header}>Hello, I'm <br />Stephen Afolayan.</h1>
+                <p className={styles.paragraph}>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p className={styles.paragraph}>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+              </div>
+              <div className={styles.projectSection}>
                 <span>James</span>
                 <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
-              <h1>Hello, I'm Stephen Afolayan.</h1>
-              <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-              </p>
-              <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-              </p>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+                <h1>Hello, I'm Stephen Afolayan.</h1>
+                <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
+                  I create amazing user facing experience with modern technologies to solve business needs.
+                </p>
+                <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
+                  I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </main>
     </div>
