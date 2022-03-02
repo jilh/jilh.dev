368750eb0a1a9a37eff251a3d54525a0cf6bb435
Afolayan Stephen <afolayan4life@gmail.com>
2022-03-02 13:03:30 +0100

    Added Talks section

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -1,60 +1,13 @@
 import styles from '../styles/ProjectSection.module.css'
 import FeaturedProjects from './FeaturedProjects'
 import OtherProjects from './OtherProjects'
+import Talks from './Talks'
 const ProjectSection = () => {
     return(
         <div className={styles.projectSection}>
             <FeaturedProjects />
             <OtherProjects />
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
-            <h1>Hello, I'm Stephen Afolayan.</h1>
-            <p>I'm a community organizer, frontend developer, and UI/UX design enthusiast.
-                I create amazing user facing experience with modern technologies to solve business needs.
-            </p>
-            <p>Whe I'm not found hitting the keyboard, speaking at events, or putting pixels in place,
-                I must be somewhere trying to fill up a 4GB space of camera memory with mouth watering pictures.
-            </p>
+            <Talks />
         </div>
     )
 }
---
+++ components/Talks.js
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import styles from '../styles/Talks.module.css';
+
+const Talks = () => {
+    return(
+        <div className={styles.talks}>
+            <b className={styles.subHeading}>TALKS</b>
+            <div className={styles.talksWrapper}>
+                <div className={styles.talksCard}>
+                    <div className={styles.cardInfo}>
+                        <div className={styles.cardTitleGroup}>
+                            <b className={styles.cardTitle}>Grachef Restaurant</b>
+                            <small className={styles.cardSubTitle}>Egbe, Kogi, Nigeria<br />29/08/2019</small>
+                        </div>
+                        <div className='image-container'>
+                            <Image src="/project-image.png" width={200} height={200} className={styles.cardImage} />
+                        </div>
+                        <p className={styles.cardDetails}>A food ordering app that brings conveniency to customers</p>
+                        <p className={styles.cardLinks}>Slides</p>
+                    </div>
+                </div>
+
+                <div className={styles.talksCard}>
+                    <div className={styles.cardInfo}>
+                        <div className={styles.cardTitleGroup}>
+                            <b className={styles.cardTitle}>Grachef Restaurant</b>
+                            <small className={styles.cardSubTitle}>Egbe, Kogi, Nigeria<br />29/08/2019</small>
+                        </div>
+                        <div className='image-container'>
+                            <Image src="/project-image.png" width={200} height={200} className={styles.cardImage} />
+                        </div>
+                        <p className={styles.cardDetails}>A food ordering app that brings conveniency to customers</p>
+                        <p className={styles.cardLinks}>Slides</p>
+                    </div>
+                </div>
+                <div className={styles.talksCard}>
+                    <div className={styles.cardInfo}>
+                        <div className={styles.cardTitleGroup}>
+                            <b className={styles.cardTitle}>Grachef Restaurant</b>
+                            <small className={styles.cardSubTitle}>Egbe, Kogi, Nigeria<br />29/08/2019</small>
+                        </div>
+                        <div className='image-container'>
+                            <Image src="/project-image.png" width={200} height={200} className={styles.cardImage} />
+                        </div>
+                        <p className={styles.cardDetails}>A food ordering app that brings conveniency to customers</p>
+                        <p className={styles.cardLinks}>Slides</p>
+                    </div>
+                </div>
+                <div className={styles.talksCard}>
+                    <div className={styles.cardInfo}>
+                        <div className={styles.cardTitleGroup}>
+                            <b className={styles.cardTitle}>Grachef Restaurant</b>
+                            <small className={styles.cardSubTitle}>Egbe, Kogi, Nigeria<br />29/08/2019</small>
+                        </div>
+                        <div className='image-container'>
+                            <Image src="/project-image.png" width={200} height={200} className={styles.cardImage} />
+                        </div>
+                        <p className={styles.cardDetails}>A food ordering app that brings conveniency to customers</p>
+                        <p className={styles.cardLinks}>Slides</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Talks
